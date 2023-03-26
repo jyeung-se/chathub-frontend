c8056dd73be7313759a694902c966b3eecc629a3
Jack Yeung <jyeung.se@gmail.com>
2023-03-26 14:53:43 -0400

    Added Chat page and messages persist on backend along with users in the group messages.

--- src/ChatMessagesPage.jsx
+++ src/ChatMessagesPage.jsx
@@ -1,5 +1,40 @@
-const ChatMessagesPage = () => {
-    return <>chat messages...</>
+// import { PrettyChatWindow } from "react-chat-engine-pretty"
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced"
+import "./App.css"
+
+// This is the react-chat-engine-advanced version - plain version
+const ChatMessagesPage = (props) => {
+
+    const chatMessageProps = useMultiChatLogic(
+        "859197cc-c802-49c9-90e7-4d53083a0a6a",
+        props.user.username,
+        props.user.secret
+    )
+
+    return (
+        <div style={{ height: '100vh' }} >
+            <MultiChatSocket {...chatMessageProps} />
+            <MultiChatWindow {...chatMessageProps} style={{ height: '100%' }} />
+        </div>
+    )
 }
+
+
+
+// This is the react-chat-engine-pretty version - it works on mobile well too, but doesn't have the side bars
+// const ChatMessagesPage = (props) => {
+//     return (
+//         <div style={{ height: '100vh' }}>
+//             <PrettyChatWindow
+//                 projectId="859197cc-c802-49c9-90e7-4d53083a0a6a"
+//                 username={props.user.username}
+//                 secret={props.user.secret}
+//                 style={{ height: '100%' }}
+//             />
+//         </div>
+//     )
+// }
+
+
 
 export default ChatMessagesPage
