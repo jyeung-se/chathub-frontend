7aa0da19b5f03e53a569d2009785d3a9eae53656
Jack Yeung <jyeung.se@gmail.com>
2023-03-26 14:10:29 -0400

    Creating user or login auth now up and running with server and backend

--- src/AuthLoginPage.jsx
+++ src/AuthLoginPage.jsx
@@ -1,10 +1,17 @@
+import axios from "axios"
+
 const AuthLoginPage = (props) => {
 
     const onSubmit = (e) => {
       e.preventDefault()
-
       const { value } = e.target[0]
-      props.onAuth({ username: value, secret: value })
+
+      axios.post(
+        "http://localhost:5000/authenticate",
+        {username: value}
+      )
+      .then(r => props.onAuth({ ...r.data, secret: value }))
+      .catch(e => console.log('error', e))
     }
   
     return (
